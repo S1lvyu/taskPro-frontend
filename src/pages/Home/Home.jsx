e3081282d1a1e6dee9ed0758ffccbfc869927c3b
Silvian Ranetescu <silvian.ranetescu@gmail.com>
2024-01-27 18:59:19 +0200

    errors solved

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import { Suspense, useEffect, useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import { Sidebar } from "../../components/Sidebar/Sidebar";
-import { Header } from "../../components/Header/Header";
+import Header from "../../components/Header/Header";
 import { Loader } from "../../components/Loader/Loader";
 import { getBoards } from "../../redux/operations";
 import { getBoardsData } from "../../redux/selectors";
 import { getUserToken } from "../../redux/selectors";
+import { useDispatch, useSelector } from "react-redux";
 
 export const Home = () => {
   const navigate = useNavigate();
